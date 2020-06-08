fe9f93eed13ab7c6e0c0871b385f5d0c256ffc83
Bruno Vukasovic <vukasovic.bruno@gmail.com>
2020-06-08 15:08:29 +0200

    Deployment

--- app/index.ts
+++ app/index.ts
@@ -36,10 +36,10 @@ app.use('/navbar', NavbarRouter);
 app.use('/page', PageRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, '../', 'client', 'build')));
+  app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
